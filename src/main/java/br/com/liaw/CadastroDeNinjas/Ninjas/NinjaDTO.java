59ff86a24ed1444e52994e3e95cbafafaa7bbd6f
LucasLiaw25 <lucasaliaw@gmail.com>
2025-07-21 11:47:44 -0300

    feat: Implementa o DTO e mapper pros ninjas

--- src/main/java/br/com/liaw/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/br/com/liaw/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package br.com.liaw.CadastroDeNinjas.Ninjas;
 
-public class NinjaDTo {
+import br.com.liaw.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private MissoesModel missoes;
+    private String rank;
+
 }
--- src/main/java/br/com/liaw/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/br/com/liaw/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,33 @@
 package br.com.liaw.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+        ninjaModel.setRank(ninjaDTO.getRank());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        ninjaDTO.setRank(ninjaModel.getRank());
+
+        return ninjaDTO;
+    }
 }
